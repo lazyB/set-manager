d3b1cd921cfc6fb0f8cf013e6e9e61b4b63c0767
Will Archer <warcher@lolofit.com>
2022-07-17 13:26:28 -0600

    Initial nav bar checkin

---
+++ app/javascript/packs/components/nav/NavBar.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import {Container, Navbar} from "react-bootstrap";
+import {Outlet} from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+
+export function LayoutWithNavbar() {
+    return (<>
+            <Navbar bg={"dark"}  variant={"dark"} fixed={"top"}>
+                <Container>
+                    <Navbar.Text>
+                        This is my first link
+                    </Navbar.Text>
+                </Container>
+                <Container>
+                    <Navbar.Text>
+                        This is my second link
+                    </Navbar.Text>
+                </Container>
+                <Container>
+                    <Navbar.Text>
+                        This is my third link
+                    </Navbar.Text>
+                </Container>
+            </Navbar>
+            <Outlet/>
+        </>
+    )
+}
